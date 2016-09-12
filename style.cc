9ec989a98f0ab46093397db47f7998724cc96616
konstantin <konstantin@new-host-2.home>
2016-09-12 14:59:06 -0400

    pull

---
+++ style.cc
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+<style>
+
+{ margin: 0; padding: 0; }
+
+#tracer{ 
+        background-size: 100% 100%;
+    	background-repeat: no-repeat;
+        no-repeat center center fixed; 
+  	background-size: contain;
+}
+
+#killdeath{ 
+        background: "killdeath.jpg" 50% no-repeat;
+        no-repeat center center fixed; 
+        background-size: cover;
+  	background-size: contain;
+}
+
+
+#banner{
+    position: center;
+    left: 9999999999px;
+    top: 9999999999px;
+    z-index: -1;
+}
+
+
+
+
+<div class="styled-select">
+.styled-select select {
+   background: transparent;
+   width: 268px;
+   padding: 5px;
+   font-size: 16px;
+   line-height: 1;
+   border: 0;
+   border-radius: 0;
+   height: 34px;
+   -webkit-appearance: none;
+   }
+</div>
+
+
+
+success {
+    width: 200px;
+    height: 100px;
+    background-color: yellow;
+    /* Rotate div */
+    -ms-transform: rotate(7deg); /* IE 9 */
+    -webkit-transform: rotate(7deg); /* Chrome, Safari, Opera */
+    transform: rotate(7deg);
+}
+
+
+
+
+.button {
+    background-color: #0099ff; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+.banner{
+height:16%;
+overflow:hidden;
+width:100%;
+}
+h1 { color: #c9d0d4; font-family: 'Helvetica Neue', sans-serif; font-size: 46px; font-weight: 100; line-height: 50px; letter-spacing: 1px; padding: 0 0 40px; border-bottom: double #555; }
+</style>
