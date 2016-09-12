a6143b9eaaf274a044d7976b7cf81cb9a2e77745
CollinMetzger <collinmeTZGEr7@gmail.com>
2016-09-11 21:24:47 -0400

    Add files via upload

--- style.cc
+++ style.cc
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 <style>
 
 { margin: 0; padding: 0; }
@@ -7,6 +16,34 @@
         no-repeat center center fixed; 
         background-size: cover;
 }
+
+#killdeath{ 
+        background: "tracer.jpg" ;
+        no-repeat center center fixed; 
+        background-size: cover;
+}
+
+<div class="styled-select">
+.styled-select select {
+   background: transparent;
+   width: 268px;
+   padding: 5px;
+   font-size: 16px;
+   line-height: 1;
+   border: 0;
+   border-radius: 0;
+   height: 34px;
+   -webkit-appearance: none;
+   }
+</div>
+
+
+
+
+
+
+
+
 .button {
     background-color: #0099ff; /* Green */
     border: none;
@@ -41,9 +78,9 @@ li a:hover {
     background-color: #111;
 }
 .banner{
-height:127px;
+height:16%;
 overflow:hidden;
+width:100%;
 }
 h1 { color: #c9d0d4; font-family: 'Helvetica Neue', sans-serif; font-size: 46px; font-weight: 100; line-height: 50px; letter-spacing: 1px; padding: 0 0 40px; border-bottom: double #555; }
 </style>
-
