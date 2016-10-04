d9b109e14320d5b809972655e58f10dd70b73669
konst4 <oleinikovk@gmail.com>
2016-10-04 01:54:28 -0400

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,46 @@
+/**
+ * Created by konstantin on 9/24/16.
+ */
+import React from "react";
+import Title from "./Header/Title";
+import Button from "./Button";
+export default class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searched: true
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+        const title = e.target.value;
+        this.props.changeTitle(title);
+    }
+
+    handleClick() {
+        this.context.router.push('/Overwatch/Search');
+    }
+
+
+    render() {
+
+    if(this.state.searched) {
+        return (
+            <div >
+                <Title title={this.props.title}/>
+                <input id="player" value="Arthas-11308" onChange={this.handleChange.bind(this)}/>
+                <Button child={this.props.child}> </Button>
+            </div>
+        );
+    }else{
+       return( <div></div>);
+    }
+
+
+    }
+}
+Header.contextTypes = {
+    router: React.PropTypes.object.isRequired
+}
