5d41b63c165d7cd6ab879f072b4ef99707249256
CollinMetzger <collinmeTZGEr7@gmail.com>
2016-10-11 22:25:11 -0400

    Add files via upload

---
+++ chartJS.js
@@ -0,0 +1,13 @@
+var bars = [];
+var figureContainer = $('<div id="figure"></div>');
+var graphContainer = $('<div class="graph"></div>');
+var barContainer = $('<div class="bars"></div>');
+var data = $(data);
+var container = $(container);
+var chartData;
+var chartYMax;
+var columnGroups;
+
+// Timer variables
+var barTimer;
+var graphTimer;
